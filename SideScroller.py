ebd84a00296b489d72e7c0a4eec698955c2ec62a
Garrett Genz <genzg@onid.orst.edu>
2017-06-05 22:21:00 -0700

    Create first loop

--- SideScroller.py
+++ SideScroller.py
@@ -1,1 +1,23 @@
-print ("Hello World!")
+import pygame
+
+screenDim = (800, 600)
+gameTitle = 'Game Title'
+
+def initializeScreen(dim):
+    pygame.init()
+    gameDisplay = pygame.display.set_mode(screenDim)
+    pygame.display.set_caption(gameTitle)
+
+initializeScreen(screenDim)
+
+pygame.display.update()
+
+gameExit = False
+
+while not gameExit:
+    for event in pygame.event.get():
+        print(event)
+
+pygame.quit()
+
+quit()
